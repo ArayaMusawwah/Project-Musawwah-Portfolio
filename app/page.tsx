5cf53c7ebe0fec2e127e188b88d49e39ef6a7122
Araya Musawwah <miramadhan354@gmail.com>
2024-06-11 15:55:50 +0700

    dev 1.0 - feat:home section(done)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import Hero from "@/components/shared/Hero"
+
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10 dark:bg-black-100">
+      <div className="w-full max-w-7xl">
+        <Hero />
+      </div>
+    </main>
+  )
 }
 export default HomePage
